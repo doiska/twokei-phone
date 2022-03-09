97f146b90c91fc9616e73ea6357885111b73d39b
doiska <doiskka@gmail.com>
2022-03-09 12:11:25 -0300

    better routing

--- web/src/Phone.tsx
+++ web/src/Phone.tsx
@@ -1,32 +1,38 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Link, Routes } from 'react-router-dom'
 import { Route } from 'react-router-dom'
 import { Apps } from '@apps/Apps'
 import HomeApp from '@apps/Home/HomeApp';
 import PhoneWrapper from './PhoneWrapper'
 import './styles/main.css'
+import NavigationBar from '@os/navigation/NavigationBar'
+import { RecoilRoot } from 'recoil'
 
-//TODO: match "/" show home app
+//TODO: https://github.com/project-error/npwd/blob/eb5678b93238638931fcce113bfcf9856832b9bd/phone/src/lib/RecoilRootManager.tsx
 
 ReactDOM.render(
     <React.StrictMode>
-        <Router>
-            <PhoneWrapper>
-                <div className="h-[90%] w-full flex flex-col">
-                    <>
-                        <Routes>
-                            <Route index element={<HomeApp />} />
-                            {
-                                Apps.map(({ id, disable, path, AppElement }) => (
-                                    <Route key={id} path={path} element={<AppElement />} />
-                                ))
-                            }
-                        </Routes>
-                    </>
-                </div>
-            </PhoneWrapper>
-        </Router>
+        <RecoilRoot>
+            <Router>
+                <PhoneWrapper>
+                    <div className="h-[95%] w-full flex flex-col">
+                        <>
+                            <Routes>
+                                <Route index element={<HomeApp />} />
+                                {
+                                    Apps.map(
+                                        ({ id, disable, path, AppElement }) =>
+                                            <Route key={id} path={path} element={<AppElement />} />
+                                    )
+                                }
+                            </Routes>
+                        </>
+                    </div>
+                    <NavigationBar />
+                </PhoneWrapper>
+            </Router>
+        </RecoilRoot>
     </React.StrictMode>,
     document.getElementById('root')
 )
