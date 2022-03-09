476c6cdc464e94642dbc61da4b793dd4d9e7114a
doiska <doiskka@gmail.com>
2022-03-09 12:11:53 -0300

    app link

--- web/src/apps/Home/HomeApp.tsx
+++ web/src/apps/Home/HomeApp.tsx
@@ -1,13 +1,14 @@
 import AppWrapper from '@ui/components/AppWrapper';
 import React from 'react';
 import { FaWhatsapp } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
     return (
-        <AppWrapper>
+        <AppWrapper style={{ backgroundImage: 'url(https://raw.githubusercontent.com/project-error/npwd/master/phone/public/media/backgrounds/minimal.jpg)' }}>
             <div className="box-content mt-6 px-1">
                 <div className="grid grid-cols-4">
-                    <FaWhatsapp fontSize={"large"} className='text-green-500' />
+                    <Link to={"whatsapp"}><FaWhatsapp fontSize={"large"} className='text-green-500' /></Link>
                 </div>
             </div>
         </AppWrapper>
