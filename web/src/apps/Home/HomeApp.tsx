6fdd8b66a7cc0839b0d0bb60a535fe0975e8df61
doiska <doiskka@gmail.com>
2022-03-10 13:06:43 -0300

    App routes

--- web/src/apps/Home/HomeApp.tsx
+++ web/src/apps/Home/HomeApp.tsx
@@ -1,14 +1,20 @@
+import { useApps } from '@os/hooks/useApp';
 import AppWrapper from '@ui/components/AppWrapper';
 import React from 'react';
 import { FaWhatsapp } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
+
+    const { apps } = useApps();
+
     return (
         <AppWrapper style={{ backgroundImage: 'url(https://raw.githubusercontent.com/project-error/npwd/master/phone/public/media/backgrounds/minimal.jpg)' }}>
-            <div className="box-content mt-6 px-1">
-                <div className="grid grid-cols-4">
-                    <Link to={"whatsapp"}><FaWhatsapp fontSize={"large"} className='text-green-500' /></Link>
+            <div className="flex flex-col mt-8 px-1 h-full w-full items-center">
+                <div className="grid grid-cols-5 text-5xl gap-3">
+                    {
+                        apps.map(({ path, icon: Icon, disable, color }) => <Link to={path}>{Icon}</Link>)
+                    }
                 </div>
             </div>
         </AppWrapper>
