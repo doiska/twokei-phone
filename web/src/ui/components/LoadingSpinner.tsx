76aa0e88544ee5beb7e2fb70abde7d3008f45f6c
doiska <doiskka@gmail.com>
2022-03-11 21:22:14 -0300

    loading suspense

---
+++ web/src/ui/components/LoadingSpinner.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingSpinner: React.FC = () => {
+    return (
+        <div className="h-full w-full flex justify-center items-center">
+            Loading...
+        </div>
+    );
+}
+
+export default LoadingSpinner;
