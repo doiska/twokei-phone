70f33ddd26ab49ccc8f879408c0920252baf8f34
doiska <doiskka@gmail.com>
2022-03-13 20:32:59 -0300

    feat(phone): added React-sentry to phone provider

--- web/src/PhoneProviders.tsx
+++ web/src/PhoneProviders.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { NotificationProvider } from '@os/notification/providers/NotificationProvider';
+import Phone from './Phone';
 
 const PhoneProviders: React.FC = ({ children }) => {
-	return <NotificationProvider>{children}</NotificationProvider>;
+	return (
+		<NotificationProvider>
+			<Suspense fallback={<div>Loading... </div>}>
+				<Phone />
+			</Suspense>
+		</NotificationProvider>
+	);
 };
 
 export default PhoneProviders;
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/main.css';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
+
 import { BrowserRouter as Router } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import PhoneProviders from 'PhoneProviders';
 import Phone from './Phone';
 
+import './styles/main.css';
+
+Sentry.init({
+	dsn: 'https://e0eff884bf5740d8940abae871bbed46@o1166650.ingest.sentry.io/6257134',
+	integrations: [new BrowserTracing()],
+
+	// Set tracesSampleRate to 1.0 to capture 100%
+	// of transactions for performance monitoring.
+	// We recommend adjusting this value in production
+	tracesSampleRate: 0.2,
+});
+
 ReactDOM.render(
 	<React.StrictMode>
-		<RecoilRoot>
-			<PhoneProviders>
-				<Router>
-					<Phone />
-				</Router>
-			</PhoneProviders>
-		</RecoilRoot>
+		<Router>
+			<RecoilRoot>
+				<PhoneProviders />
+			</RecoilRoot>
+		</Router>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
