7cae06a698bb87e7f4af3340efdd8d97550fb37c
doiska <doiskka@gmail.com>
2022-03-13 20:32:25 -0300

    ci(project): added react-sentry to dev dependencies

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,5 +1,14 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    daisyui: {
+        themes: [
+            {
+                phone: {
+                    "base-100": "#27272a"
+                }
+            }
+        ]
+    },
 	theme: {
 		extend: {
 			width: {
